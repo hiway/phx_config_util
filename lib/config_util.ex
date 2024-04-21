e9cdfb9f122e6bb99031ea7cfeba56c8ee03f096
Harshad Sharma <harshad@sharma.io>
2024-04-21 11:43:37 +0530

    Run "mix new" and add net_address as dependency

---
+++ lib/config_util.ex
@@ -0,0 +1,5 @@
+defmodule ConfigUtil do
+  @moduledoc """
+   Utility functions to configure Elixir/ Phoenix apps.
+  """
+end
---
+++ mix.exs
@@ -0,0 +1,40 @@
+defmodule ConfigUtil.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :config_util,
+      description: description(),
+      package: package(),
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:net_address, "~> 0.3.1"}
+    ]
+  end
+
+  defp description() do
+    "Utility functions to configure Elixir/ Phoenix apps."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hiway/phx_config_util/"}
+    ]
+  end
+end
